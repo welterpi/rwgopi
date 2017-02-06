87f0e9baa31c1aaef5b03c62ff816825ef328ad8
welterpi <welterlenr@gmail.com>
2017-02-05 21:47:10 -0600

    Update distance.py

--- distance.py
+++ distance.py
@@ -6,31 +6,34 @@ GPIO.setmode(GPIO.BCM)
 TRIG = 13
 ECHO = 26
 
-print "Distance Measurement in Progress"
+define Distance (TRIG,ECHO)
+  print "Distance Measurement in Progress"
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
+  GPIO.setup(TRIG,GPIO.OUT)
+  GPIO.setup(ECHO,GPIO.IN)
 
-GPIO.output(TRIG, False)
-print "Waiting for Sensor"
-time.sleep(2)
+  GPIO.output(TRIG, False)
+  print "Waiting for Sensor"
+  time.sleep(2)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+  GPIO.output(TRIG, True)
+  time.sleep(0.00001)
+  GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
+  while GPIO.input(ECHO)==0:
+    pulse_start = time.time()
 
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+  while GPIO.input(ECHO)==1:
+    pulse_end = time.time()
 
-pulse_duration = pulse_end - pulse_start
+  pulse_duration = pulse_end - pulse_start
 
-distance = pulse_duration * 17150
+  distance = pulse_duration * 17150
 
-distance = round(distance, 2)
+  distance = round(distance, 2)
 
-print "Distance: ",distance,"cm"
+  print "Distance: ",distance,"cm"
 
-GPIO.cleanup()
+  GPIO.cleanup()
+  
+return(distance)
