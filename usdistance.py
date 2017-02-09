93595b6350e8156a6b24a7b0167f57bb0e3d1af7
welterpi <welterlenr@gmail.com>
2017-02-08 18:52:38 -0600

    usdistance.py
    Python 3 program for HC-SR04 Ultrasonic Sensor using polling
    #Output (ping) on pin 11, input (listen) on pin 33, pin 6 GND
    #5Vcc pin must go to RPi pin 2, trig pin to RPi pin 11
    #Echo MUST go through voltage divider circuit !!!!
    #Echo -> 4.7k Ohm resistor -> RPi pin 13 + 10k Ohm -> RPi & SR0-

---
+++ usdistance.py
@@ -0,0 +1,5 @@
+#Python 3 program for HC-SR04 Ultrasonic Sensor using polling
+#Output (ping) on pin 11, input (listen) on pin 33, pin 6 GND
+#5Vcc pin must go to RPi pin 2, trig pin to RPi pin 11
+#Echo MUST go through voltage divider circuit !!!!
+#Echo -> 4.7k Ohm resistor -> RPi pin 13 + 10k Ohm -> RPi & SR0-
